df2bb1de349b12da0a1358e12b0f331a5fb18e26
apbnscouts <apbnscoutsit@gmail.com>
2024-12-01 21:36:40 +0600

    Update page.tsx

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -15,7 +15,6 @@ export const metadata: Metadata = {
 export default async function Home() {
   return (
     <main className="w-full max-w-screen-xl mx-auto px-2 md:px-0">
-      <Notice />
       <Hero />
       <Stat />
       <News />
